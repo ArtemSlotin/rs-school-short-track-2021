f93420be9cd4c463024cffa7eda4c732d6d11ac1
Artem Slotin <66305683+ArtemSlotin@users.noreply.github.com>
2021-04-12 23:20:41 +0300

    inplement solution #11

--- src/11-encode-line.js
+++ src/11-encode-line.js
@@ -8,8 +8,23 @@
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new Error('Not implemented');
+function encodeLine(str) {
+  const arr = str.split('');
+  let letters = [];
+  let count = 1;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === arr[i + 1]) {
+      count++;
+    } else if (!(arr[i + 1] === arr[i]) && count > 1) {
+      letters.push(`${count}${arr[i]}`);
+      count = 1;
+    } else {
+      const same = `${arr[i]}`;
+      letters = [...letters, ...same];
+      count = 1;
+    }
+  }
+  return letters.join('');
 }
 
 module.exports = encodeLine;
